3c7592c395421ccc860b9089644d28659dcc23a2
omarkhaledabdo <omarkhaledabdo@gmail.com>
2017-08-31 20:57:56 +0200

    .

--- model/userAction.js
+++ model/userAction.js
@@ -5,28 +5,22 @@ const users = mongoose.model('user');
 const interactor = require('../interactor');
 
 
-
-
 /* To add User Details to message */
 function notifyArrival (ID) {
     console.log("notify arrival");
     users.findOne({ID: ID}, function (err, user) {
        var currentEvent = user.currentEvent;
-
-
        console.log("currentEvent " +  currentEvent);
 
        if(currentEvent == null){
            return;
        }
 
-
        events.findOne({_id: currentEvent}, function (err, event) {
            event.studentIDs.forEach(function(recipient) {
               if(true || recipient != user.ID) {
                   var instructorType = event.type == 'lecture' ? 'doctor' : 'teacher assistant';
-                  var messageText = user.firstName + " " + user.lastName + ' reported that the ';
-                  instructorType + 'arrived!';
+                  var messageText = user.firstName + " " + user.lastName + ' reported that the ' + instructorType + 'arrived!';
                    interactor.sendTextMessage(recipient, messageText);
                }
            });
--- roomInformer.js
+++ roomInformer.js
@@ -9,57 +9,51 @@ var eventControl = require('./model/eventControl');
 var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 
-function isVowel (c) {
+function isVowel(c) {
     return "aeoiu".indexOf(c) != -1;
 
 }
 
+function run() {
+    console.log("Run scheduler");
 
-    function run(){
-        console.log("Run scheduler");
+    var rule = new schedule.RecurrenceRule();
 
-        var rule = new schedule.RecurrenceRule();
+    rule.dayOfWeek = [new schedule.Range(0, 6)];
 
-        rule.dayOfWeek = [new schedule.Range(0, 6)];
+    rule.hour = [new schedule.Range(0, 23)];
 
-        rule.hour = [new schedule.Range(0, 23)];
+    rule.minute = [new schedule.Range(0, 59)];
 
-        rule.minute = [new schedule.Range(0, 59)];
-
-        rule.second = [new schedule.Range(0, 59, 30)]; //start, end, step
-
-
-        schedule.scheduleJob(rule, function () {
-            var curTime = (function() {
-                var curTime = new Date();
-                return weekdays[curTime.getDay()] + '-' + curTime.getHours();
-            })();
-
-            console.log("CurTime " + curTime  + "\n\n");
-
-            var cursor = events.find({date: curTime}).cursor();
-
-            cursor.on('data', function (event) {
-                console.log(event);
-                event.studentIDs.forEach(function(recipient) {
-
-                    var messageText = 'You have ' + (isVowel(event.name.charAt(0)) ? 'an ' : 'a ') +  event.name +
-                        ' ' + event.type +  ' in room ' + event.room
-                    console.log(messageText)
-                    fb.sendTextMessage(recipient, messageText);
-
-                    //console.log("EventID " + event._id + "\n");
-                    users.findOneAndUpdate({ID: recipient}, {currentEvent: event._id}).exec();
-                    //userControl.setUsersState(doc.group);
-                });
+    rule.second = [new schedule.Range(0, 59, 30)]; //start, end, step
 
 
+    schedule.scheduleJob(rule, function () {
+        var curTime = (function () {
+            var curTime = new Date();
+            return weekdays[curTime.getDay()] + '-' + curTime.getHours();
+        })();
 
+        console.log("CurTime " + curTime + "\n\n");
+
+        var cursor = events.find({date: curTime}).cursor();
+
+        cursor.on('data', function (event) {
+            console.log(event);
+            event.studentIDs.forEach(function (recipient) {
+
+                var messageText = 'You have ' + (isVowel(event.name.charAt(0)) ? 'an ' : 'a ') + event.name +
+                    ' ' + event.type + ' in room ' + event.room
+                console.log(messageText)
+                fb.sendTextMessage(recipient, messageText);
+
+                //console.log("EventID " + event._id + "\n");
+                users.findOneAndUpdate({ID: recipient}, {currentEvent: event._id}).exec();
+                //userControl.setUsersState(doc.group);
             });
-
         });
-    }
+    });
+}
 
 
-
-    module.exports.run = run;
+module.exports.run = run;
