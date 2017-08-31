cf892eddbc2a29988bd98ad511990e601412459d
hazefully <hazem.essam@gmail.com>
2017-08-31 20:27:32 +0200

    fix syntax

--- roomInformer.js
+++ roomInformer.js
@@ -6,23 +6,38 @@ var users = mongoose.model('user');
 var userControl = require('./model/userControl.js');
 var eventControl = require('./model/eventControl');
 
-
 var weekdays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 
-function isVowel(c) {
+function isVowel (c) {
     return "aeoiu".indexOf(c) != -1;
 
 }
 
-function run() {
-    console.log("Run scheduler");
 
-    var rule = new schedule.RecurrenceRule();
+    function run(){
+        console.log("Run scheduler");
 
-    rule.dayOfWeek = [new schedule.Range(0, 6)];
+        var rule = new schedule.RecurrenceRule();
 
-    rule.hour = [new schedule.Range(0, 23)];
+        rule.dayOfWeek = [new schedule.Range(0, 6)];
+
+        rule.hour = [new schedule.Range(0, 23)];
+
+        rule.minute = [new schedule.Range(0, 59)];
+
+        rule.second = [new schedule.Range(0, 59, 30)]; //start, end, step
+
+
+        schedule.scheduleJob(rule, function () {
+            var curTime = (function() {
+                var curTime = new Date();
+                return weekdays[curTime.getDay()] + '-' + curTime.getHours();
+            })();
+
+            console.log("CurTime " + curTime  + "\n\n");
+
+            var cursor = events.find({date: curTime}).cursor();
 
             cursor.on('data', function (event) {
                 console.log(event);
@@ -38,28 +53,13 @@ function run() {
                     //userControl.setUsersState(doc.group);
                 });
 
-    rule.second = [new schedule.Range(0, 59, 30)]; //start, end, step
 
 
-    schedule.scheduleJob(rule, function () {
-        var curTime = (function () {
-            var curTime = new Date();
-            return weekdays[curTime.getDay()] + '-' + curTime.getHours();
-        })();
-        console.log("CurTime " + curTime + "\n\n");
-        var cursor = events.find({date: curTime}).cursor();
-        cursor.on('data', function (event) {
-            console.log(event);
-            event.studentIDs.forEach(function (recipient) {
-                var messageText = 'You have ' + isVowel(even.name.charAt(0)) ? 'an ' : 'a ' + event.name +
-                    ' ' + event.type + ' in room ' + event.room + '\n';
-                fb.sendTextMessage(recipient, messageText);
-                //console.log("EventID " + event._id + "\n");
-                users.findOneAndUpdate({ID: recipient}, {currentEvent: event._id}).exec();
-                //userControl.setUsersState(doc.group);
             });
-        });
-    });
-}
 
-module.exports.run = run;
+        });
+    }
+
+
+
+    module.exports.run = run;
