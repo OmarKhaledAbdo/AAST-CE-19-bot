9b3190ae1d3e7cac313d0dabf5f01ddb600b0494
omarkhaledabdo <omarkhaledabdo@gmail.com>
2017-08-31 20:43:18 +0200

    .

--- roomInformer.js
+++ roomInformer.js
@@ -44,7 +44,7 @@ function isVowel (c) {
                 event.studentIDs.forEach(function(recipient) {
 
                     var messageText = 'You have ' + (isVowel(event.name.charAt(0)) ? 'an ' : 'a ') +  event.name +
-                        ' ' + event.type +  ' in room' + event.room
+                        ' ' + event.type +  ' in room ' + event.room
                     console.log(messageText)
                     fb.sendTextMessage(recipient, messageText);
 
