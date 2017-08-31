4439b552163733de851e4cf31f34413786e4af96
hazefully <hazem.essam@gmail.com>
2017-08-31 20:22:00 +0200

    merging

--- model/eventControl.js
+++ model/eventControl.js
@@ -18,11 +18,7 @@ function addUserToCourse(ID, name, group) {
 
 function loadSchedule() {
     addEvent("Web Engineering", "lecture", "349", "Thursday-19");  //Sunday-8
-<<<<<<< HEAD
     addEvent("Web Engineering", "lab", "G310", "Sunday-19");
-=======
-    addEvent("Web Engineering", "lab", "G310", "Sunday-14");
->>>>>>> cec8ca9f01a5a77f9cf0d54066adb865fa7739db
 
     // addEvent("Computing Algorithms", "lecture", "351", "Tuesday-10");
     // addEvent("Computing Algorithms", "section", "441", "Monday-12");
